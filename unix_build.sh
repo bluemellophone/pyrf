c5a8e4b63e418b54cab23f34c6cc12c675beb856
Jason Parham <bluemellophone@gmail.com>
2015-10-28 11:39:00 -0400

    Updated build script with otool call to fix mac

--- unix_build.sh
+++ unix_build.sh
@@ -31,6 +31,9 @@ echo 'Building with make'
 export NCPUS=$(grep -c ^processor /proc/cpuinfo)
 make -j$NCPUS -w
 #################################
+echo 'Fixing OSX libiomp'
+install_name_tool -change libiomp5.dylib ~/code/libomp_oss/exports/mac_32e/lib.thin/libiomp5.dylib lib*
+#################################
 echo 'Moving the shared library'
 cp -v lib* ../pyrf
 cd ..
